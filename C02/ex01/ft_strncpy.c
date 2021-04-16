0e6f55cc5d5141fd68d21fa7e9138e062fba0ea4
hbaldino <hbaldino@student.42sp.org.br>
2021-04-16 14:49:02 +0000

    c02v1.1

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbaldino <hbaldino@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/14 00:23:57 by hbaldino          #+#    #+#             */
+/*   Updated: 2021/04/16 14:00:34 by hbaldino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
